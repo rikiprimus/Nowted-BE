6a138492150c65220cf174df6d2360745c725857
rikiprimus <rikiprimus33@gmail.com>
2024-07-19 19:51:00 +0700

    first commit

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import cors from 'cors';
+// import cors from 'cors';
 import helmet from 'helmet';
 import bodyParser from 'body-parser';
 import Router from './src/routes/index.js';
