ec40b65831db8698693ce042bbc8b5341a9541b3
rikiprimus <rikiprimus33@gmail.com>
2024-07-19 19:45:58 +0700

    first commit

--- index.js
+++ index.js
@@ -14,13 +14,7 @@ const app = express();
 const allowedOrigins = ['http://localhost:5173', 'http://localhost:5173/home', 'https://nowted-app-six.vercel.app'];
 
 const corsOptions = {
-  origin: (origin, callback) => {
-    if (allowedOrigins.includes(origin) || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
+  origin: '*',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
